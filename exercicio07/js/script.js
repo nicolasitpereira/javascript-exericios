cf7ed22b3f93617cd7e385f7078a2225bbcd1cf2
nicolasitpereira <nicolas.pereira.it@gmail.com>
2024-09-29 12:44:09 -0300

    Create script.js

---
+++ exercicio07/js/script.js
@@ -0,0 +1,12 @@
+let valor = prompt("Digite um valor:");
+
+valor = Number(valor);
+
+if (valor > 10) {
+    alert("É MAIOR QUE 10!");
+} else 
+    if (valor < 10){
+    alert("NÃO É MAIOR QUE 10!");
+} else {
+    alert("É IGUAL A 10")
+}
